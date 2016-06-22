2f40f188d40499df7738188d7eb50d91ba10c068
Paul <huangpg@59store.com>
2016-06-22 15:48:37 +0800

    Title:解决5.0以上不能访问service的bug

--- AidlClient/app/src/main/java/com/example/aidlclient/MainActivity.java
+++ AidlClient/app/src/main/java/com/example/aidlclient/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.aidlclient;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,9 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private IDog baseService;
 
     private IGetMsg customService;
-    private static User[] users = new User[] {
-            new User(0, "jack0", "99999999990",""),
-            new User(1, "jack0", "99999999990","")
+    private static User[] users = new User[]{
+            new User(0, "jack0", "99999999990", ""),
+            new User(1, "jack0", "99999999990", "")
     };
 
     @Override
@@ -81,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void start() {
         Intent intent = new Intent();
         intent.setAction("com.example.aidlserver.BASE_SERVICE");
-        bindService(intent, connBase, BIND_AUTO_CREATE);
+        bindService(createExplicitFromImplicitIntent(getApplicationContext(), intent), connBase, BIND_AUTO_CREATE);
         Toast.makeText(MainActivity.this, "开始绑定服务", Toast.LENGTH_SHORT).show();
     }
 
@@ -108,9 +112,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
-
-
     private ServiceConnection customConn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -122,21 +123,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             customService = null;
         }
     };
+
     public void start_custom() {
         Intent intent = new Intent();
         intent.setAction("com.example.aidlserver.CUSTOM_SERVICE");
-        bindService(intent, customConn, BIND_AUTO_CREATE);
+        bindService(createExplicitFromImplicitIntent(getApplicationContext(), intent), customConn, BIND_AUTO_CREATE);
         Toast.makeText(MainActivity.this, "开始绑定服务", Toast.LENGTH_SHORT).show();
     }
 
     public void get_custom() {
         try {
-            Random random=new Random();
-            int nextInt=random.nextInt(2);
-            List<Message2> msgs=customService.getMsg(users[nextInt]);
-            StringBuilder sBuilder=new StringBuilder();
-            for(Message2 msg:msgs){
-                sBuilder.append(msg.toString()+"\n");
+            Random random = new Random();
+            int nextInt = random.nextInt(2);
+            List<Message2> msgs = customService.getMsg(users[nextInt]);
+            StringBuilder sBuilder = new StringBuilder();
+            for (Message2 msg : msgs) {
+                sBuilder.append(msg.toString() + "\n");
             }
             Toast.makeText(MainActivity.this, sBuilder.toString(), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
@@ -149,4 +151,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         unbindService(customConn);
         Toast.makeText(MainActivity.this, "解除绑定服务成功", Toast.LENGTH_SHORT).show();
     }
+
+
+    /**
+     * 解决android L之后的bindservice失败的问题
+     * @param context
+     * @param implicitIntent
+     * @return
+     */
+    public static Intent createExplicitFromImplicitIntent(Context context, Intent implicitIntent) {
+        // Retrieve all services that can match the given intent
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfo = pm.queryIntentServices(implicitIntent, 0);
+        // Make sure only one match was found
+        if (resolveInfo == null || resolveInfo.size() != 1) {
+            return null;
+        }
+
+        // Get component info and create ComponentName
+        ResolveInfo serviceInfo = resolveInfo.get(0);
+        String packageName = serviceInfo.serviceInfo.packageName;
+        String className = serviceInfo.serviceInfo.name;
+        ComponentName component = new ComponentName(packageName, className);
+
+        // Create a new intent. Use the old one for extras and such reuse
+        Intent explicitIntent = new Intent(implicitIntent);
+
+        // Set the component to be explicit
+        explicitIntent.setComponent(component);
+
+        return explicitIntent;
+    }
+
 }
